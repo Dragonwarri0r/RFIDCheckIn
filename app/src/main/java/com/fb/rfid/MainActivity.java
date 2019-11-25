8e670b2fd6e5c1c531f19d2e90bdf063502566b4
Dragonwarri0r <1121118610@qq.com>
2019-11-25 18:53:31 +0800

    Initial commit

---
+++ app/src/main/java/com/fb/rfid/MainActivity.java
@@ -0,0 +1,42 @@
+package com.fb.rfid;
+
+import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    Toast.makeText(MainActivity.this,"home",Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.navigation_add:
+                    Toast.makeText(MainActivity.this,"dash",Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.navigation_view:
+                    Toast.makeText(MainActivity.this,"view",Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+}
