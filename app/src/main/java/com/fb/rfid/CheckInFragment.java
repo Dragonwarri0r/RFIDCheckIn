386f75b52f9f0938cfba84566d3c1f18e83f4923
dragonwarrior <1121118610@qq.com>
2019-11-26 00:12:47 +0800

    ADD First Fragment

---
+++ app/src/main/java/com/fb/rfid/CheckInFragment.java
@@ -0,0 +1,36 @@
+package com.fb.rfid;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CheckInFragment extends Fragment {
+
+    private CheckInViewModel mViewModel;
+
+    public static CheckInFragment newInstance() {
+        return new CheckInFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.check_in_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(CheckInViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/fb/rfid/CheckInViewModel.java
@@ -0,0 +1,7 @@
+package com.fb.rfid;
+
+import androidx.lifecycle.ViewModel;
+
+public class CheckInViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
